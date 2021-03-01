a9491ce3a9abfb0aaa2dbaf3bc9335a85896f0de
reisson <reisson.saavedra.miguel@gmail.com>
2021-03-01 04:57:16 -0500

    adding data

---
+++ src/main/java/AppConfigs.java
@@ -0,0 +1,7 @@
+public class AppConfigs {
+    final public static String appID = "START_KAFKA";
+    final static String bootstrapServer = "localhost:9092";
+    final public static String topicName = "input-topic-demo";
+    final public static String stateStoreName = "state-store";
+    final public static String stateStoreLocation = "tmp/state-store";
+}
---
+++ src/main/java/KTableAggDemo.java
@@ -0,0 +1,23 @@
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+
+import java.util.Properties;
+
+public class KTableAggDemo {
+
+//    private Properties streamProps(String bootstrapServer){
+//        Properties props = new Properties();
+//        props.put(StreamsConfig.APPLICATION_ID_CONFIG, AppConfigs.appID);
+//        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServer);
+//        props.put(StreamsConfig.STATE_DIR_CONFIG, AppConfigs.stateStoreLocation);
+//        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG,100);
+//        return props;
+//    }
+//
+//    StreamsBuilder streamBuilder = new StreamsBuilder();
+//    streamBuilder.table(AppConfigs.topicName,Consumed.with(App))
+
+}
